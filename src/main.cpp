27dd8432d69ea9dd9d784201540ed91c7017cde3
Han Young <hanyoung@protonmail.com>
2020-04-29 14:56:31 +0800

    now it compiles

--- src/main.cpp
+++ src/main.cpp
@@ -15,30 +15,33 @@
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QApplication app(argc, argv);
-    QQmlApplicationEngine engine;
+  QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QApplication app(argc, argv);
+  QQmlApplicationEngine engine;
 
-    KLocalizedString::setApplicationDomain("kweather");
-    engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
-    KAboutData aboutData("kweather", "Weather", "0.1", "Weather application in Kirigami", KAboutLicense::GPL, i18n("© 2020 KDE Community"));
-    KAboutData::setApplicationData(aboutData);
+  KLocalizedString::setApplicationDomain("kweather");
+  engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
+  KAboutData aboutData("kweather", "Weather", "0.1",
+                       "Weather application in Kirigami", KAboutLicense::GPL,
+                       i18n("© 2020 KDE Community"));
+  KAboutData::setApplicationData(aboutData);
 
-    // initialize models in context
-    auto *weatherLocationListModel = new WeatherLocationListModel();
-    
-    engine.rootContext()->setContextProperty("weatherLocationListModel", weatherLocationListModel);
-    
-    // register QML types
-    qmlRegisterType<WeatherLocation>("kweather", 1, 0, "WeatherLocation");
-    qmlRegisterType<WeatherDay>("kweather", 1, 0, "WeatherDay");
-    qmlRegisterType<WeatherHour>("kweather", 1, 0, "WeatherHour");
-    
-    engine.load(QUrl(QStringLiteral("qrc:///qml/main.qml")));
+  // initialize models in context
+  auto *weatherLocationListModel = new WeatherLocationListModel();
 
-    if (engine.rootObjects().isEmpty()) {
-        return -1;
-    }
+  engine.rootContext()->setContextProperty("weatherLocationListModel",
+                                           weatherLocationListModel);
 
-    return app.exec();
+  // register QML types
+  qmlRegisterType<WeatherLocation>("kweather", 1, 0, "WeatherLocation");
+  qmlRegisterType<WeatherDay>(     "kweather", 1, 0, "WeatherDay");
+  qmlRegisterType<WeatherHour>(    "kweather", 1, 0, "WeatherHour");
+
+  engine.load(QUrl(QStringLiteral("qrc:///qml/main.qml")));
+
+  if (engine.rootObjects().isEmpty()) {
+    return -1;
+  }
+
+  return app.exec();
 }
--- src/weatherlocationmodel.cpp
+++ src/weatherlocationmodel.cpp
@@ -2,73 +2,75 @@
 #include "weatherdaymodel.h"
 
 /* ~~~ WeatherLocation ~~~ */
+WeatherLocation::WeatherLocation() {}
 
-WeatherLocation::WeatherLocation(QString locationName, float latitude, float longitude)
+WeatherLocation::WeatherLocation(QString locationName,
+                                 float   latitude,
+                                 float   longitude)
 {
-    this->locationName_ = locationName;
-    this->latitude_ = latitude;
-    this->longitude_ = longitude;
-    this->weatherDayListModel_ = new WeatherDayListModel(this);
-    this->weatherHourListModel_ = new WeatherHourListModel(this);
+  this->locationName_         = locationName;
+  this->latitude_             = latitude;
+  this->longitude_            = longitude;
+  this->weatherDayListModel_  = new WeatherDayListModel(this);
+  this->weatherHourListModel_ = new WeatherHourListModel(this);
 }
 
-WeatherLocation::WeatherLocation(QString locationName, float latitude, float longitude, QList<AbstractWeatherForecast*> forecasts)
+WeatherLocation::WeatherLocation(QString                         locationName,
+                                 float                           latitude,
+                                 float                           longitude,
+                                 QList<AbstractWeatherForecast *>forecasts)
 {
-    this->locationName_ = locationName;
-    this->latitude_ = latitude;
-    this->longitude_ = longitude;
-    this->forecasts_ = forecasts;
-    this->weatherDayListModel_ = new WeatherDayListModel(this);
-    this->weatherHourListModel_ = new WeatherHourListModel(this);
+  this->locationName_         = locationName;
+  this->latitude_             = latitude;
+  this->longitude_            = longitude;
+  this->forecasts_            = forecasts;
+  this->weatherDayListModel_  = new WeatherDayListModel(this);
+  this->weatherHourListModel_ = new WeatherHourListModel(this);
 }
 
 /* ~~~ WeatherLocationListModel ~~~ */
+WeatherLocationListModel::WeatherLocationListModel(QObject *parent) {}
 
-int WeatherLocationListModel::rowCount(const QModelIndex &parent) const
+int WeatherLocationListModel::rowCount(const QModelIndex& parent) const
 {
-    Q_UNUSED(parent);
-    return locationsList.size();
+  Q_UNUSED(parent);
+  return locationsList.size();
 }
 
-QVariant WeatherLocationListModel::data(const QModelIndex &index, int role) const
+QVariant WeatherLocationListModel::data(const QModelIndex& index, int role) const
 {
-    return QVariant();
+  return QVariant();
 }
-
 
 void WeatherLocationListModel::updateUi()
 {
-    emit dataChanged(createIndex(0, 0), createIndex(locationsList.count() - 1, 0));
+  emit dataChanged(createIndex(0, 0), createIndex(locationsList.count() - 1, 0));
 }
 
-void WeatherLocationListModel::insert(int index, WeatherLocation* weatherLocation)
+void WeatherLocationListModel::insert(int index, WeatherLocation *weatherLocation)
 {
-    if (index < 0 || index > locationsList.count())
-        return;
-    
-    emit beginInsertRows(QModelIndex(), index, index);
-    locationsList.insert(index, weatherLocation);
-    emit endInsertRows();
+  if ((index < 0) || (index > locationsList.count())) return;
+
+  emit beginInsertRows(QModelIndex(), index, index);
+  locationsList.insert(index, weatherLocation);
+  emit endInsertRows();
 }
 
 void WeatherLocationListModel::remove(int index)
 {
-    if (index < 0 || index >= locationsList.count())
-        return;
-    
-    emit beginRemoveRows(QModelIndex(), index, index);
-    locationsList.removeAt(index);
-    emit endRemoveRows();
+  if ((index < 0) || (index >= locationsList.count())) return;
+
+  emit beginRemoveRows(QModelIndex(), index, index);
+  locationsList.removeAt(index);
+  emit endRemoveRows();
 }
 
-WeatherLocation* WeatherLocationListModel::get(int index)
+WeatherLocation * WeatherLocationListModel::get(int index)
 {
-    if (index < 0 || index >= locationsList.count())
-        return new WeatherLocation("", 0, 0);
-    
-    return locationsList.at(index);
+  if ((index < 0) || (index >= locationsList.count())) return new WeatherLocation(
+      "",
+      0,
+      0);
+
+  return locationsList.at(index);
 }
-
-
-
-
