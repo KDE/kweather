592cc4aa48dc1337a9b50c070fd46a59567752e7
Han Young <hanyoung@protonmail.com>
2020-05-22 09:39:28 +0800

    add REUSE

--- src/abstractsunrise.h
+++ src/abstractsunrise.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2020 Han Young <hanyoung@protonmail.com>
+ * Copyright 2020 Devin Lin <espidev@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 #ifndef ABSTRACTSUNRISE_H
 #define ABSTRACTSUNRISE_H
 
